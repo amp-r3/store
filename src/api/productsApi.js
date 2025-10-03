6109d6acefae78446bd1a06d44564700b7d2d47f
amp-r3 <amp_r3@proton.me>
2025-10-03 13:53:51 +0500

    feat: added the ability to sort products on the page

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -4,16 +4,24 @@ const apiClient = axios.create({
     baseURL: 'https://dummyjson.com'
 });
 
-export const fetchProducts = (page) => {
+export const fetchProducts = (params) => {
+    const { page, sortBy, order } = params;
     const limit = 12;
     const skip = (page - 1) * limit;
-    return apiClient.get(`/products?limit=${limit}&skip=${skip}`)
+
+    let url = `/products?limit=${limit}&skip=${skip}`;
+
+    if (sortBy && order) {
+        url += `&sortBy=${sortBy}&order=${order}`;
+    }
+
+    return apiClient.get(url);
 }
 
 export const fetchProductsById = (product) => {
-    return apiClient.get(`/products/${product}`)
+    return apiClient.get(`/products/${product}`);
 }
 
 export const searchProducts = (query) => {
-    return apiClient.get(`/products/search?q=${query}`)
+    return apiClient.get(`/products/search?q=${query}`);
 }
