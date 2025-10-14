fc2d137c209394903db8a785478b67c81d7f7d16
Amir <ergashevamir06@gmail.com>
2025-10-14 22:04:33 +0500

    style(Navbar): adapted the design for small screens

--- src/components/ui/NoResults/NoResults.jsx
+++ src/components/ui/NoResults/NoResults.jsx
@@ -6,7 +6,7 @@ const NoResults = () => {
     <div className={style.noResults}>
         <h2 className={style.noResults__title}>No results found</h2>
         <p className={style.noResults__desc}>Try changing your search query.</p>
-        <button onClick={() => window.location.reload(false) && navigate('/')} className={style.noResults__btn}>Go to catalog</button>
+        <button onClick={() => navigate('/')} className={style.noResults__btn}>Go to catalog</button>
     </div>
   )
 }
