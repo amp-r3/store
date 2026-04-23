a0cafb9487c33db170670f365c54a570aa8b66e4
amp-r3 <amp_r3@proton.me>
2026-04-23 12:26:44 +0500

    feat(router): add ProtectedRoute component

---
+++ src/components/layout/ProtectedRoute/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import { Navigate, useLocation } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { JSX } from 'react'
+import { selectIsAuth } from '@/store/selectors/authSelectors'
+
+
+export const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
+  const isAuth = useSelector(selectIsAuth)
+  const location = useLocation()
+
+  if (!isAuth) {
+    return <Navigate to="/login" state={{ from: location.pathname }} replace />
+  }
+
+  return children
+}
