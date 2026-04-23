c3cf04dc8ea1078e3bd8012042630c66d33dbf49
amp-r3 <amp_r3@proton.me>
2026-04-23 15:05:44 +0500

    feat(ui): add styles for FormField component

--- src/components/common/FormField/FormField.tsx
+++ src/components/common/FormField/FormField.tsx
@@ -1,4 +1,5 @@
-import { InputHTMLAttributes, forwardRef } from "react";
+import { InputHTMLAttributes, forwardRef, useId } from "react";
+import style from './form-field.module.scss'
 
 interface FormFieldProps extends InputHTMLAttributes<HTMLInputElement> {
   label: string;
@@ -6,16 +7,34 @@ interface FormFieldProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 export const FormField = forwardRef<HTMLInputElement, FormFieldProps>(
-  ({ label, error, ...props}, ref) => (
-    <div>
-      <label>{label}</label>
-      <input 
-      ref={ref}
-      {...props} 
-      />
-      {error && <span>{error}</span>}
-    </div>
-  )
+  ({ label, error, id, className, ...props }, ref) => {
+    const generatedId = useId();
+    const inputId = id || generatedId;
+    const errorId = `${inputId}-error`;
+
+    return (
+      <div className={style.wrapper}>
+        <label htmlFor={inputId} className={style.label}>
+          {label}
+        </label>
+
+        <input
+          id={inputId}
+          ref={ref}
+          className={`${style.input} ${error ? style.inputError : ''} ${className || ''}`}
+          aria-invalid={!!error}
+          aria-describedby={error ? errorId : undefined}
+          {...props}
+        />
+
+        {error && (
+          <span id={errorId} className={style.error} role="alert">
+            {error}
+          </span>
+        )}
+      </div>
+    )
+  }
 )
 
-FormField.displayName = 'FormField'
+FormField.displayName = 'FormField' 
