0dea130f38624d84d7ae2d0f171288b83dd4b1d6
amp-r3 <amp_r3@proton.me>
2026-04-30 13:15:21 +0500

    feat: Add a new button ShareCopyBtn to common and use it in ProductHeader

--- src/pages/ProductPage/components/ProductHeader/ProductHeader.tsx
+++ src/pages/ProductPage/components/ProductHeader/ProductHeader.tsx
@@ -1,5 +1,6 @@
 import { BackButton } from '@/components/common/BackButton/BackButton';
-
+import { ShareCopyBtn } from '@/components/common/ShareCopyBtn/ShareCopyBtn';
+import style from './product-header.module.scss'
 interface ProductHeaderProps {
     onClick: () => void;
     label?: string;
@@ -7,8 +8,9 @@ interface ProductHeaderProps {
 
 export const ProductHeader = ({ onClick, label }: ProductHeaderProps) => {
     return (
-        <div>
+        <div className={style['product-header']}>
             <BackButton onClick={onClick} label={label} />
+            <ShareCopyBtn />
         </div>
     );
 };
