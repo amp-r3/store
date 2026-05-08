af78ad668800a98d3635c0c64872085100c4552c
amp-r3 <amp_r3@proton.me>
2026-05-08 15:04:17 +0500

    refactor(Layout): extract supabase auth listener into useAuthSync hook
    Remove onAuthStateChange useEffect from Layout component.
    Auth synchronization logic now lives in hooks/features/auth/useAuthSync.

--- src/components/layout/Layout/Layout.tsx
+++ src/components/layout/Layout/Layout.tsx
@@ -1,92 +1,31 @@
 import { useNavigation, Outlet } from 'react-router';
-import { Suspense, useEffect } from 'react';
+import { Suspense } from 'react';
 
 // Custom Components
 import { Navbar } from '../Navbar/Navbar';
 import { CartDrawer } from '@/components/cart';
 import { MobileBar } from '../MobileBar/MobileBar';
 import { Loader } from '@/components/common';
+import { Footer } from '../Footer/Footer';
+import { Header } from '../Header/Header';
 
 // Redux Hooks
-import { useAppDispatch, useAppSelector } from '@/hooks/redux';
+import { useAppDispatch, useAppSelector } from '@/hooks';
+
 // Functions and Selectors
-import { clearCart, closeCart } from '@/store/slices/cartSlice';
+import { closeCart } from '@/store/slices/cartSlice';
 import { selectIsCartOpen } from '@/store/selectors/cartSelectors';
 
 // Style
 import style from './layout.module.scss';
-import { Footer } from '../Footer/Footer';
-import { useTheme } from '@/hooks';
-import { Header } from '../Header/Header';
-import { supabase } from '@/supabase';
-import { logout, setSession } from '@/store/slices/authSlice';
-import { store } from '@/app/store';
-import { cartApi, useSyncCartMutation } from '@/services/cartApi';
-import { useSyncWishlistMutation, wishlistApi } from '@/services/wishlistApi';
-import { clearFavorite } from '@/store/slices/wishlistSlice';
-
+import { useAuthSync } from '@/hooks';
 
 export const Layout = () => {
-    useTheme();
     const navigation = useNavigation();
-    const isLoading = navigation.state === 'loading';
     const dispatch = useAppDispatch();
+    useAuthSync();
     const isOpen = useAppSelector(selectIsCartOpen);
-    const [syncCart] = useSyncCartMutation();
-    const [syncWishlist] = useSyncWishlistMutation();
-
-    useEffect(() => {
-        const { data: { subscription } } = supabase.auth.onAuthStateChange(
-            async (event, session) => {
-                if (session?.user) {
-                    dispatch(setSession({
-                        user: {
-                            id: session.user.id,
-                            firstName: session.user.user_metadata.firstName,
-                            lastName: session.user.user_metadata.lastName,
-                            username: session.user.user_metadata.username,
-                            email: session.user.email!,
-                            accessToken: session.access_token,
-                        },
-                        token: session.access_token,
-                    }));
-                }
-
-                if (event === 'SIGNED_IN' && session?.user) {
-                    const currentState = store.getState();
-                    const localWishlistItems = currentState.wishlist.favoriteItems
-                    const localCartItems = currentState.cart.items;
-
-                    if (localCartItems && Object.keys(localCartItems).length > 0) {
-                        try {
-                            await syncCart(localCartItems).unwrap();
-                            dispatch(clearCart());
-                        } catch (error) {
-                            console.error('Error synchronizing cart:', error);
-                        }
-                    }
-
-                    if (localWishlistItems && Object.keys(localWishlistItems).length > 0) {
-                        try {
-                            await syncWishlist(localWishlistItems).unwrap();
-                            dispatch(clearFavorite());
-                        } catch (error) {
-                            console.error('Error synchronizing wishlist:', error);
-                        }
-                    }
-                }
-
-                else if (event === 'SIGNED_OUT') {
-                    dispatch(cartApi.util.resetApiState());
-                    dispatch(wishlistApi.util.resetApiState());
-                    dispatch(clearFavorite())
-                    dispatch(clearCart());
-                    dispatch(logout());
-                }
-            });
-
-        return () => subscription.unsubscribe();
-    }, [dispatch, syncCart]);
+    const isLoading = navigation.state === 'loading';
 
     const handleClose = () => {
         dispatch(closeCart())
