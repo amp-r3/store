e96ba7a76dc1a4da64c98037e15d9ede914d1777
amp-r3 <amp_r3@proton.me>
2026-05-08 13:13:24 +0500

    feat(hooks): add wishlist facade hooks for unified state management

---
+++ src/hooks/useWishlistActions.ts
@@ -0,0 +1,29 @@
+import { selectIsAuth } from "@/store/selectors/authSelectors"
+import { useAppDispatch, useAppSelector } from "./redux"
+import { useGetWishlistQuery, useToggleWishlistMutation } from "@/services/wishlistApi"
+import { useCallback } from "react"
+import { toogleFavorite } from "@/store/slices/wishlistSlice"
+
+interface useWishlistActionsProps {
+  onWishlist(id: number): void;
+  isUpdating: boolean;
+}
+
+export const useWishlistActions = (): useWishlistActionsProps => {
+  const dispatch = useAppDispatch()
+  const isAuth = useAppSelector(selectIsAuth)
+
+  const { data: wishlistData } = useGetWishlistQuery(undefined, { skip: !isAuth });
+
+  const [toggleWishlist, { isLoading: isToggling }] = useToggleWishlistMutation();
+
+  const onWishlist = useCallback((id: number) => {
+    if (isAuth) {
+      toggleWishlist({ productId: id, isInWishlist: !!wishlistData?.[id] })
+    } else {
+      dispatch(toogleFavorite(id))
+    }
+  }, [isAuth, toggleWishlist, dispatch, wishlistData])
+
+  return { onWishlist, isUpdating: isToggling }
+}
--- src/hooks/useWishlistDetails.ts
+++ src/hooks/useWishlistDetails.ts
@@ -1,8 +1,10 @@
 import { useMemo } from 'react';
-import { useSelector } from 'react-redux';
 import { useProductsByIds } from './useProductByIds';
 import { Product } from '@/types/products';
 import { WishlistData, selectFavoritesArray } from '@/store/selectors/wishlistSelectors';
+import { useAppSelector } from './redux';
+import { selectIsAuth } from '@/store/selectors/authSelectors';
+import { useGetWishlistQuery } from '@/services/wishlistApi';
 
 interface WishlistDetailsReturn {
   wishlistDetails: Product[];
@@ -13,22 +15,38 @@ interface WishlistDetailsReturn {
   isEmpty: boolean;
 }
 
-export const useWishlistDetails = (isOpen: boolean = true): WishlistDetailsReturn => {
-  const wishlistItems = useSelector(selectFavoritesArray);
-  
+export const useWishlistDetails = (): WishlistDetailsReturn => {
+  const isAuth = useAppSelector(selectIsAuth)
+  const localWishlistItems = useAppSelector(selectFavoritesArray);
+
+  const { data, isLoading: isWishlistLoading, isError: isWishlistError, isFetching: isWishlistFetching } = useGetWishlistQuery(undefined, { skip: !isAuth });
+
+  const unifiedWishlistItems = useMemo(
+    () => {
+      if (isAuth && data) {
+        return (Object.entries(data) as [string, boolean][]).map(
+          ([id]) => ({
+            id: Number(id)
+          })
+        );
+      }
+      return localWishlistItems
+    }, [isAuth, data, localWishlistItems]);
+
+
   const productIds = useMemo(
-    () => wishlistItems.map(item => item.id), 
-    [wishlistItems]
+    () => unifiedWishlistItems.map(item => item.id),
+    [unifiedWishlistItems]
   );
 
-  const { products, isLoading, isError, isFetching } = useProductsByIds(productIds, isOpen);
+  const { products, isLoading: isProductsLoading, isFetching: isProductsFetching, isError: isProductsError } = useProductsByIds(productIds);
 
   return {
-    wishlistDetails: products, 
-    wishlistItems,                  
-    isLoading, 
-    isFetching,
-    isError,
-    isEmpty: wishlistItems.length === 0,
+    wishlistDetails: products,
+    wishlistItems: unifiedWishlistItems,
+    isLoading: isWishlistLoading || isProductsLoading,
+    isFetching: isWishlistFetching || isProductsFetching,
+    isError: isWishlistError || isProductsError,
+    isEmpty: unifiedWishlistItems.length === 0,
   };
 };
