6aa242ed944bb77704bf9fe6cc00354add0622e3
amp-r3 <amp_r3@proton.me>
2026-05-23 16:41:15 +0500

    refactor(checkout): switch guard to server-side cart fetching

--- src/components/layout/CheckoutGuard/CheckoutGuard.tsx
+++ src/components/layout/CheckoutGuard/CheckoutGuard.tsx
@@ -1,44 +1,29 @@
 import { Loader } from '@/components/common';
-import { useAppSelector, useCartDetails } from '@/hooks';
+import { useAppSelector } from '@/hooks';
 import { useGetCartQuery } from '@/services/cartApi';
 import { selectIsAuth } from '@/store/selectors/authSelectors';
-import { CartProduct } from '@/store/selectors/cartSelectors';
-import { CartItem } from '@/types/products';
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
-import { Header } from '../Header/Header';
 
 
 export const CheckoutGuard = () => {
   const location = useLocation();
+  const isAuth = useAppSelector(selectIsAuth);
 
-  const isAuth = useAppSelector(selectIsAuth)
-
-  const { isLoading, isFetching, isEmpty, cartDetails } = useCartDetails()
-
-
-  if (isLoading) {
-    return (
-      <div>
-        <Loader />
-      </div>
-    );
-  }
+  const { data, isLoading, isFetching } = useGetCartQuery(undefined, {
+    skip: !isAuth,
+  });
 
   if (!isAuth) {
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
-  if (!isFetching && isEmpty) {
+  if (isLoading || isFetching) {
+    return <Loader />;
+  }
+
+  if (!data || Object.keys(data).length === 0) {
     return <Navigate to="/" replace />;
   }
 
-  const hasOutOfStockItems = cartDetails.some((item) => item.availabilityStatus === 'Out of stock');
-  if (hasOutOfStockItems) {
-    return <Navigate to="/" replace />;
-  }
-
-  return (
-    <Outlet />
-  )
-
+  return <Outlet />;
 };
