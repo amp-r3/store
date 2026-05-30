221153fa640ae24ceb953e5e864671c94f621b50
amp-r3 <amp_r3@proton.me>
2026-05-30 13:40:43 +0500

    refactor(config): expand payment config into order-management module
    Rename payment.config to order-management.config.tsx and include new specific configurations for statuses, deliveries, and payments. Fix entrypoint imports.

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,7 @@
 export { PROVIDER_CONFIG } from './providers.config'
-export { PAYMENT_CONFIG } from './payment.config'
+export {
+    PAYMENT_CONFIG,
+    DELIVERY_CONFIG,
+    PAYMENT_STATUS_MAP,
+    DELIVERY_STATUS_MAP
+} from './order-management.config'
