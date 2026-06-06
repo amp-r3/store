d40b1a766d5fd0cd3e22d68354a8eb7e4130aa2b
amp-r3 <amp_r3@proton.me>
2026-06-06 10:13:36 +0500

    style(components): adapt AddToCartButton styles and skeleton for alignment with QuickBuyButton

--- src/components/common/AddToCartButton/AddToCartButtonSkeleton.tsx
+++ src/components/common/AddToCartButton/AddToCartButtonSkeleton.tsx
@@ -1,10 +1,32 @@
 import Skeleton from "react-loading-skeleton";
+import styles from "./add-to-cart-button.module.scss";
 
-export const AddToCartButtonSkeleton = () => {
+interface AddToCartButtonSkeletonProps {
+  className?: string;
+}
+
+export const AddToCartButtonSkeleton = ({ className = "" }: AddToCartButtonSkeletonProps) => {
   return (
-    <Skeleton
-      height={54}
-      borderRadius={18}
-    />
+    <div
+      className={`${styles.container} ${className}`}
+      style={{
+        background: "transparent",
+        borderColor: "transparent",
+        boxShadow: "none",
+        pointerEvents: "none",
+      }}
+    >
+      <Skeleton
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          borderRadius: "var(--radius-lg, 12px)",
+          display: "block",
+        }}
+      />
+    </div>
   );
 };
